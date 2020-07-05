977cd8532da44a17da6d859aaa0a2d37f2af49be
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2020-07-05 19:04:02 +0200

    Formatted prediction.

--- real_estate_app/app.py
+++ real_estate_app/app.py
@@ -1,3 +1,5 @@
+from real_estate_model import ModelPipeline
+
 from bokeh.io import curdoc
 from bokeh.plotting import figure
 from bokeh.tile_providers import CARTODBPOSITRON_RETINA
@@ -10,6 +12,8 @@ from bokeh.models import Select
 from bokeh.models import TextInput
 
 import requests
+
+import pandas as pd
 
 import math
 
@@ -34,8 +38,19 @@ class RealEstateApp:
     def __init__(self):
         """Initialize the app."""
 
+        self.model = ModelPipeline.load()
+
         self.zipcodes = ['8000', '8200', '8210', '8230']
+
+        self.latitude_min = 56.13
+        self.latitude_max = 56.18
+        self.longitude_min = 10.18
+        self.longitude_max = 10.26
+
         self.selected_zipcode = None
+        self.selected_road = None
+        self.selected_address = None
+        self.selected_prediction = None
 
         # Startup.
         self.layout = row(
@@ -54,22 +69,48 @@ class RealEstateApp:
         )
         zipcode.on_change(
             'value',
-            self.zipcode_handler
+            self.zipcode_handler,
         )
         search = column(
             children=[zipcode]
         )
         if self.selected_zipcode is not None:
-            address = Select(
+            road = Select(
                 title="Vej:",
-                value='value',
-                options=self.get_roads(),
+                value=self.selected_road,
+                options=sorted(list(self.roads)),
             )
-            search.children.append(address)
+            road.on_change(
+                'value',
+                self.road_handler,
+            )
+            search.children.append(road)
+            if self.selected_road is not None:
+                address = Select(
+                    title="Addresse:",
+                    value=self.selected_address,
+                    options=sorted(list(self.addresses)),
+                )
+                address.on_change(
+                    'value',
+                    self.address_handler,
+                )
+                search.children.append(address)
+                if self.selected_prediction is not None:
+                    formatted_price = (
+                        f'{round(self.selected_prediction):,}'
+                        .format(',', '.')
+                    )
+                    predicted_price = Div(
+                        text=(
+                            f"<p>Predicted price: {formatted_price} DKK</p>"
+                        )
+                    )
+                    search.children.append(predicted_price)
 
         # Range bounds supplied in web mercator coordinates.
-        range0 = merc(56.13, 10.18)
-        range1 = merc(56.18, 10.26)
+        range0 = merc(self.latitude_min, self.longitude_min)
+        range1 = merc(self.latitude_max, self.longitude_max)
         aarhus_map = figure(
             x_range=(range0[0], range1[0]),
             y_range=(range0[1], range1[1]),
@@ -85,21 +126,69 @@ class RealEstateApp:
         ]
 
     def zipcode_handler(self, attr, old, new):
+        self.selected_road = None
+        self.selected_address = None
+        self.selected_prediction = None
         self.selected_zipcode = new
+        self.get_roads()
         self.refresh_plots()
 
-    def get_roads(self):
-        return sorted(set(map(
-            lambda response: response['vejstykke']['navn'],
+    def road_handler(self, attr, old, new):
+        self.selected_road = new
+        self.selected_address = None
+        self.selected_prediction = None
+        self.get_addresses()
+        self.refresh_plots()
+
+    def address_handler(self, attr, old, new):
+        self.selected_address = new
+        self.selected_prediction = self.predict()
+        print(self.selected_prediction)
+        self.refresh_plots()
+
+    def get_addresses(self):
+        self.addresses = dict(map(
+            lambda response: (response['adressebetegnelse'], response),
             requests.get(
-                'https://dawa.aws.dk/adgangsadresser',
+                'https://dawa.aws.dk/adresser',
+                params={
+                    'navngivenvej_id': self.roads[self.selected_road]['id'],
+                }
+            ).json(),
+        ))
+
+    def get_roads(self):
+        self.roads = dict(map(
+            lambda response: (response['navn'], response),
+            requests.get(
+                'https://dawa.aws.dk/navngivneveje',
                 params={
                     'postnr': self.selected_zipcode,
                 }
             ).json(),
-        )))
+        ))
+
+    def predict(self, addressid=None) -> float:
+        if addressid is None:
+            addressid = self.addresses[self.selected_address]['id']
+
+        bbr_response = requests.get(
+            'https://dawa.aws.dk/bbrlight/enheder',
+            params={
+                'adresseid': addressid,
+            }
+        )
+        if bbr_response.status_code == 200:
+            bbr = bbr_response.json()[0]
+            return float(self.model.predict({
+                'rooms': bbr['VAERELSE_ANT'],
+                'size': bbr['BEBO_ARL'],
+                'build_year': bbr['BEBO_ARL'],
+                'latitude':  10,
+                'longitude': 50,
+            }))
 
 
 planning_tool = RealEstateApp()
 curdoc().add_root(planning_tool.layout)
-curdoc().title = "Planning tool"
+curdoc().title = "Real Estate Model"
